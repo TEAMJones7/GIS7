6f0c3a3998e4a5d25e286ee1ac054bede1aee126
Fransiscus Ivan M Sinaga <fransiscussinaga0608@gmail.com>
2018-10-23 12:30:36 -0700

    HARI TERAKHIR

---
+++ 1164039_FRANSISCUS_TUGAS1/Soal10/soal10.py
@@ -0,0 +1,16 @@
+import shapefile #mengimpor modul shapefil
+w=shapefile.Writer() #untuk membuat shapefile baru
+w.shapeType #menyeting menggunakan jenis shape apa (point,polygon)
+#membuat dbs dengan 2 field, berupa kolom
+w.field("Nama Bidang","Bidang Ke")
+w.field("Nama Bidang","Bidang Ke")
+#membuat dbs dengan 2 field, berupa kolom
+w.record("Segitiga Siku-Siku","satu")
+w.record("Segitiga Siku-Siku","dua")
+w.record("Segitiga Siku-Siku","tiga")
+
+#membuat 3 row karena menggunakan 3
+w.poly(parts=[[[-7,7],[-1,7], [-7,1],[-7,7]]],shapeType=shapefile.POLYGON) #mengisi .shp dengan titik point
+w.poly(parts=[[[7,7],[1,7], [7,1],[7,7]]],shapeType=shapefile.POLYGON) #mengisi .shp dengan titik point
+w.poly(parts=[[[7,-7],[1,-7], [7,-1],[7,-7]]],shapeType=shapefile.POLYGON) #mengisi .shp dengan titik point
+w.save("soal10")#melakukan save dengan nama (soal1)
